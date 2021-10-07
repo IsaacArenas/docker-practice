141a0b5f835f95cd07580ea8cf18f4a9e22c1064
Isaac Arenas <isaacarenas@Isaacs-Mac-mini.local>
2021-10-07 17:03:34 -0400

    making db connection to flask

--- database/set_db.sql
+++ database/set_db.sql
@@ -1,8 +1,11 @@
-CREATE USER 'admin' IDENTIFIED BY 'juice$Boxes';
+CREATE USER 'admin' IDENTIFIED BY 'juiceBoxes';
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';
 CREATE DATABASE WORKSHOP;
 use WORKSHOP;
 CREATE TABLE images( id int , url varchar(255), name varchar(255), vertical boolean);
 INSERT INTO images VALUES 
 (1, "https://isaacarenasphotography.s3.amazonaws.com/places/barkyoto.jpg", "pontocho", true ),
-(2, "https://isaacarenasphotography.s3.amazonaws.com/places/themet.jpg", "themet", true);
+(2, "https://isaacarenasphotography.s3.amazonaws.com/places/themet.jpg", "the Met", false),
+(3, "https://isaacarenasphotography.s3.amazonaws.com/portrait/pic+of+the+pic.jpg", "pic of the pic", true),
+(4, "https://isaacarenasphotography.s3.amazonaws.com/places/chicagoTheatre.jpg", "chicago theatre", false),
+(5, "https://isaacarenasphotography.s3.amazonaws.com/places/lostintranslation.jpg", "lost in translation", false);
--- flask-project/main.py
+++ flask-project/main.py
@@ -1,7 +1,43 @@
 from flask import Flask
+import mysql.connector
+import os
+import json
 
 app = Flask(__name__)
+env = os.environ
+DB_USER= env.get('DB_USER')
+DB_PASS= env.get('DB_USER_PASS')
+DB_HOST= env.get('DB_HOST')
+DB_NAME= env.get('DB_NAME')
+config = {
+  'user': DB_USER,
+  'password': DB_PASS,
+  'host': DB_HOST,
+  'database': DB_NAME
+}
 
-@app.route("/images")
-def hello_world():
-    return "<p>Hello, World!</p>"
+    
+@app.route("/images", methods = ['POST', 'GET'])
+def getImages():
+    print(config)
+    cnx = mysql.connector.connect(**config)
+    cursorA = cnx.cursor()
+    cursorA.execute("SELECT * FROM images")
+    result = cursorA.fetchall()
+    cnx.close()
+    resultJson =json.dumps(parseResults(result))
+    return resultJson
+
+def parseResults(resultList):
+    results=[]
+    for item in resultList:
+        results.append({
+            'id': item[0],
+            'url': item[1],
+            'name':item[2], 
+            'vertical': item[3]==1
+        })
+    return results
+    
+    
+
